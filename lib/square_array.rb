48ba89678efaeaa226d27b53bd6f4f75c92cbed8
Alex S Grimes <alex@alexgrimes.com>
2020-04-13 18:18:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   i = 0
   square_array = []
   while i < 3 do
-    square_array.push array[i] ** array[i] 
+    square_array.push array[i] ** 2 
     i += 1
   end
   square_array
