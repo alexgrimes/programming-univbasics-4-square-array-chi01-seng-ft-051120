214b68702a71e2bc805190fd5e031f30ecab42e8
Alex S Grimes <alex@alexgrimes.com>
2020-04-13 18:05:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,20 @@
 def square_array(array)
-  # your code here
+  i = 0
+  array = [1, 2, 3]
+  square_array = []
+  while i < array.length do
+    square_array array[i] **
+    i += 1
+  end
+  square_array
 end
+
+#def return_string_array(array)
+  #count = 0
+  #new_array = []
+  #while count < array.length do
+    #new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+    #count += 1
+  #end
+  #new_array
+#end
